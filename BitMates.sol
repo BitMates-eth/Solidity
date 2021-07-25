07c063932a9a2888e4e1e68c5456177cdff2a87c
0xFlayed <70045544+0xFlayed@users.noreply.github.com>
2021-07-25 12:37:52 -0400

    Updated addresses from test wallets.

--- BitMates.sol
+++ BitMates.sol
@@ -1912,14 +1912,10 @@ contract BitMates is ERC721, Ownable {
 
     bool public saleIsActive = false;
     
-    // withdraw addresses
-    //address t1 = 0x2ae929FD768bE055d5B39eb8140F5Ba537362fC8;
-   //address t2 = 0xCe43925a55c876e7904C3ea262b29Eecd0D0109b;
+    //withdraw addresses
+    address t1 = 0x2ae929FD768bE055d5B39eb8140F5Ba537362fC8;
+    address t2 = 0xCe43925a55c876e7904C3ea262b29Eecd0D0109b;
    
-     // withdraw addresses
-    address t1 = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-   address t2 = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-
 
     constructor(string memory name, string memory symbol, uint256 maxNftSupply) ERC721(name, symbol) {
         maxBitMates = maxNftSupply;
